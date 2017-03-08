c985a5ece9d6a77ffaf5fd74406449a5382917c0
Jeremy Wildfire <jwildfire@gmail.com>
2017-03-08 10:23:14 -0800

    remove log/linear toggle

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -94,14 +94,7 @@ export const controlInputs = [
         values: null, // set in syncControlInputs()
         require: true
     },
-    {
-        type: 'radio',
-        label: 'Y-axis scale:',
-        option: 'y.type',
-        values: ['linear','log'], // set in syncControlInputs()
-        relabels: ['Linear','Log'],
-        require: true
-    },
+   
     {
         type: 'radio',
         label: 'Hide visits with no data:',
