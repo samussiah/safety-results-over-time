59fd12bd972f6adfe7f48b35a526737f86db898e
samussiah <samussiah@gmail.com>
2019-03-27 16:16:04 -0400

    update cdn in test-page

--- test-page/index.js
+++ test-page/index.js
@@ -1,24 +1,20 @@
 d3.csv(
     'https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv',
-    function(d,i) {
+    function(d) {
         return d;
     },
-    function(error,data) {
-        if (error)
-            console.log(error);
-
-        var settings = {
-            groups: [
-                {value_col: 'SEX', label: 'Sex'},
-                {value_col: 'ARM', label: 'Treatment Group'},
-                {value_col: 'RACE', label: 'Race'},
-                {value_col: 'SITEID', label: 'Site'},
-            ],
-            color_by: 'ARM',
-        };
+    function(data) {
         var instance = safetyResultsOverTime(
-            '#container',
-            settings
+            '#container', // element
+            {
+                groups: [
+                    {value_col: 'SEX', label: 'Sex'},
+                    {value_col: 'ARM', label: 'Treatment Group'},
+                    {value_col: 'RACE', label: 'Race'},
+                    {value_col: 'SITEID', label: 'Site'},
+                ],
+                color_by: 'ARM',
+            } // settings
         );
         instance.init(data);
     }
